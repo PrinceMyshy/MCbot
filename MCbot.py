a54027d0ba14e4e1fccbd3b803183d09d5a679d6
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-05-07 17:32:48 -0400

    praw debug?

--- MCbot.py
+++ MCbot.py
@@ -1,14 +1,14 @@
-import praw
-import time
-import random
+import praw 
 import os
 
+reddit = praw.Reddit(client_id='A50qwu2QUzy0lKpsbI7evg',
+                     client_secret='1XKUOioVjx5OhnPS-df_d2GxnRRBPA',
+                     user_agent='<console:MAGNIFICENT_C:1.0>')
+
+subreddit = reddit.subreddit("MagnificentCentury")
+
+'''
 client_id = os.getenv('client_id')
 client_secret = os.getenv('client_secret')
 user_agent = os.getenv('user_agent')
-
-reddit = praw.Reddit(client_id='client_id',
-                     client_secret='client_secret',
-                     user_agent='user_agent')
-
-subreddit = reddit.subreddit("MagnificentCentury")
+'''
