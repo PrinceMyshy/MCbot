1001fa7c7d2ad51cd5963ece5e2bb36784076cde
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-05-07 15:36:53 -0400

    adding subreddit

--- MCbot.py
+++ MCbot.py
@@ -1,7 +1,14 @@
 import praw
 import time
 import random
+import os
 
-reddit = praw.Reddit(client_id='client',
-                     client_secret='secret',
-                     user_agent='agent')
+client_id = os.getenv('client_id')
+client_secret = os.getenv('client_secret')
+user_agent = os.getenv('user_agent')
+
+reddit = praw.Reddit(client_id='client_id',
+                     client_secret='client_secret',
+                     user_agent='user_agent')
+
+subreddit = reddit.subreddit("MagnificentCentury")
