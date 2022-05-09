e18c28cc77da107388709a31f0b725f3b66d7b6a
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-05-09 18:26:46 -0400

    Magc sub

--- MCbot.py
+++ MCbot.py
@@ -5,7 +5,7 @@ reddit = praw.Reddit(client_id='A50qwu2QUzy0lKpsbI7evg',
                      client_secret='1XKUOioVjx5OhnPS-df_d2GxnRRBPA',
                      user_agent='<console:MAGNIFICENT_C:1.0>')
 
-subreddit = reddit.subreddit("MagnificentCentury")
+subreddit = reddit.subreddit("MagC_bot")
 
 print("this bot now works")
 
